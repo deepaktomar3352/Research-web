bf4a1e413e18def70342e7be2cec96aea4719cd5
Deepak Singh Tomar <112108549+deepaktomar3352@users.noreply.github.com>
2024-05-07 14:34:32 +0530

    remove render method apply navigation in paper submit

--- src/admin/Dashboard.js
+++ src/admin/Dashboard.js
@@ -1,6 +1,5 @@
 import { React, useState } from "react";
 import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import MuiDrawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import MuiAppBar from "@mui/material/AppBar";
@@ -10,9 +9,6 @@ import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import Badge from "@mui/material/Badge";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
 import Link from "@mui/material/Link";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
