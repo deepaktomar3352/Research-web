f5b798e7b872e8aba15df87df66c55559ff16312
Rohittomar01 <rohittomar9285@gmail.com>
2024-06-26 19:43:30 +0530

    did home page responsive

--- src/Components/StarterComponent.js
+++ src/Components/StarterComponent.js
@@ -14,18 +14,18 @@ export default function StarterComponent() {
           {/* Featured paper components can be dynamically rendered here */}
           <div className="paper-card">
             <div className="paper-cardChild">
-            <div className="paper-details">
-              <div>
-                <span
-                  style={{
-                    color: "#fff",
-                    fontFamily: "sans-serif",
-                    fontSize: "2.7rem",
-                  }}
-                >
-                  Empowering Minds Through{" "}
-                </span>
-                {/* <span
+              <div className="paper-details">
+                <div>
+                  <span
+                    style={{
+                      color: "#fff",
+                      fontFamily: "sans-serif",
+                      fontSize: "2.7rem",
+                    }}
+                  >
+                    Empowering Minds Through{" "}
+                  </span>
+                  {/* <span
                   style={{
                     color: "#fff",
                     fontFamily:"sans-serif",
@@ -34,29 +34,29 @@ export default function StarterComponent() {
                 >
                   Your 
                 </span> */}
-                <span
-                  style={{
-                    color: "#FF6A3D",
-                    fontFamily: "sans-serif",
-                    fontSize: "2.7rem",
-                  }}
-                >
-                  Research
-                </span>
-              </div>
-              <p>
-                Through rigorous investigation and analysis, research lays the
-                groundwork for advancements in science, technology, medicine,
-                and more.
-              </p>
+                  <span
+                    style={{
+                      color: "#FF6A3D",
+                      fontFamily: "sans-serif",
+                      fontSize: "2.7rem",
+                    }}
+                  >
+                    Research
+                  </span>
+                </div>
+                <p>
+                  Through rigorous investigation and analysis, research lays the
+                  groundwork for advancements in science, technology, medicine,
+                  and more.
+                </p>
 
-              <Link style={{ textDecoration: "none" }} to={"/SignIn"}>
-                <button className="get-started-btn">Get Started</button>
-              </Link>
-            </div>
-            <div className="paper-image">
-              <img src={img} alt="Paper" />
-            </div>
+                <Link style={{ textDecoration: "none" }} to={"/SignIn"}>
+                  <button className="get-started-btn">Get Started</button>
+                </Link>
+              </div>
+              <div className="paper-image">
+                <img src={img} alt="Paper" />
+              </div>
             </div>
           </div>
         </div>
